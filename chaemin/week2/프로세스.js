a36643f535dde57d961553110e5ffcdb98486938
chamny20 <dge3179@ajou.ac.kr>
2025-04-12 01:35:40 +0900

    week2/programmers: 프로세스

---
+++ chaemin/week2/프로세스.js
@@ -0,0 +1,21 @@
+function solution(priorities, location) {
+  const n = priorities.length;
+  const arr = Array.from(Array(n), (_, idx) => idx);
+
+  let max = priorities[0];
+  let ans = 0;
+
+  while (priorities.length) {
+    max = Math.max(...priorities);
+
+    if (max > priorities[0]) {
+      priorities.push(priorities.shift());
+      arr.push(arr.shift());
+    } else {
+      ans++;
+      priorities.shift();
+      if (arr.shift() === location) return ans;
+    }
+  }
+  return ans;
+}
