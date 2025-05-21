897bc5cfdec9dbffb60a2414f488c4fa0dc87e50
@u_limit_ <76727686+urimeee@users.noreply.github.com>
2025-05-21 14:42:05 +0900

    week5/yurim: 최소직사각형 문제 풀이 추가

---
+++ yurim/week5/최소직사각형.js
@@ -0,0 +1,23 @@
+function solution(sizes) {
+    const arrangedSizes = sizes.map((size) => {
+        if(size[0] < size[1]) {
+            let temp = size[0]
+            size[0] = size[1];
+            size[1] = temp;
+            return size;
+        }
+        return size;
+    })
+
+    const width = [];
+    const height = [];
+    arrangedSizes.forEach((size) => {
+        width.push(size[0]);
+        height.push(size[1]);
+    })
+
+    const maxWidth = width.reduce((a,b)=>Math.max(a,b), -Infinity);
+    const maxheight = height.reduce((a,b)=>Math.max(a,b), -Infinity)
+
+    return maxWidth * maxheight;
+}
