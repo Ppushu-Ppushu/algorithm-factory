245d5c228186ae154aae976018b834c7561ebfc1
chamny20 <dge3179@ajou.ac.kr>
2025-05-21 20:55:29 +0900

    week5/programmers: 카펫

---
+++ chaemin/week5/카펫.js
@@ -0,0 +1,17 @@
+function solution(brown, yellow) {
+  // b + y = 12
+  const sum = brown + yellow;
+  let ans = [0, 0];
+
+  for (let h = 3; h <= Math.sqrt(sum); h++) {
+    if (sum % h === 0) {
+      const w = sum / h;
+      const innerW = w - 2;
+      const innerH = h - 2;
+
+      if (innerW * innerH === yellow) ans = [w, h];
+    }
+  }
+
+  return ans;
+}
