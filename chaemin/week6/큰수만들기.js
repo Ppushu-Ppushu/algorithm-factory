4a3efb3341e28fac767d1b52a00aae3de97eef3b
chamny20 <dge3179@ajou.ac.kr>
2025-06-12 00:04:37 +0900

    week6/programmers: 큰 수 만들기

---
+++ chaemin/week6/큰수만들기.js
@@ -0,0 +1,14 @@
+function solution(number, k) {
+  const stack = [];
+
+  for (let i = 0; i < number.length; i++) {
+    const num = Number(number[i]);
+
+    while (stack.length > 0 && k > 0 && stack.at(-1) < num) {
+      stack.pop();
+      k--;
+    }
+    stack.push(num);
+  }
+  return stack.slice(0, number.length - k).join("");
+}
