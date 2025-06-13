e653c75a963416595c9c877cfec9beb0591bbe33
chamny20 <dge3179@ajou.ac.kr>
2025-06-14 01:38:15 +0900

    week6/programmers: 구명보트

---
+++ chaemin/week6/구명보트.js
@@ -0,0 +1,18 @@
+function solution(people, limit) {
+  let ans = 0;
+
+  people.sort((a, b) => a - b);
+
+  while (people.length > 0) {
+    if (people[0] + people.at(-1) <= limit) {
+      people.shift();
+      people.pop();
+      ans++;
+    } else {
+      people.pop();
+      ans++;
+    }
+  }
+
+  return ans;
+}
